06017bdc691fc15383902b0cd4f1b6f316bc5dab
Mustafa OZCAN <ozcanmustafa@gmail.com>
2015-01-26 00:18:43 +0200

    test

--- app/src/main/java/m2/com/myapplication/MainActivity.java
+++ app/src/main/java/m2/com/myapplication/MainActivity.java
@@ -1,27 +1,23 @@
 package m2.com.myapplication;
 
 import android.app.Activity;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.content.Context;
-import android.os.Build;
-import android.os.Bundle;
-import android.view.Gravity;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.support.v4.widget.DrawerLayout;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 
+    //test eklendi
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
